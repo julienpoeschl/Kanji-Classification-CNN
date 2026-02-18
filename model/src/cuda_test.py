e93313b312530937ab04accce44191c660e751fa
julienpoeschl <julienpoeschl@outlook.de>
2026-02-18 22:25:00 +0100

    Whitespace

--- model/src/cuda_test.py
+++ model/src/cuda_test.py
@@ -5,6 +5,7 @@ Run this file to quickly check if pytorch is using cuda and the target device wa
 """
 
 import torch
+
 print(torch.__version__)
 print(torch.cuda.is_available())
 print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU found")
