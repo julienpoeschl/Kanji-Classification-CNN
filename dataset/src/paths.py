96646102ee857b4851ea3819a1bed826e2b918c8
julienpoeschl <julienpoeschl@outlook.de>
2026-02-19 11:55:52 +0100

    Added paths for prcessed in- and output data

--- dataset/src/paths.py
+++ dataset/src/paths.py
@@ -2,6 +2,8 @@ import os
 
 DATA_DIR = os.path.join("dataset", "data")
 PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed_data")
+PROCESSED_INPUTS_PATH = os.path.join(PROCESSED_DATA_DIR, "processed_inputs.npz")
+PROCESSED_OUTPUTS_PATH = os.path.join(PROCESSED_DATA_DIR, "processed_outputs.npz")
 LABEL_MAP_PATH = os.path.join(PROCESSED_DATA_DIR, "processed_output_map.json")
 
 KANJI_DATA_PATH = os.path.join(DATA_DIR, "kanji_1000_extra.json")
